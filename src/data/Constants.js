c4953495eb8f9ddb16dd9ce2f45b030a26b6d493
ElixirCalicut <132209133+ElixirCalicut@users.noreply.github.com>
2026-06-23 17:50:19 +0530

    Update Constants.js

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -4,8 +4,8 @@
 // export const IMAGE_BASEURL="https://rishraj.pythonanywhere.com";
 // export const BASE_URL="http://localhost:8000/client/api/json/v1/1";
 // export const IMAGE_BASEURL="http://localhost:8000";
-export const BASE_URL="https://ad147snookercafe.elixirofo.com/client/api/json/v1/1";
-export const IMAGE_BASEURL="https://ad147snookercafe.elixirofo.com";
+export const BASE_URL="https://adblissfultastes.elixirofo.com/client/api/json/v1/1";
+export const IMAGE_BASEURL="https://ad1blissfultastes.elixirofo.com";
 
 
 
