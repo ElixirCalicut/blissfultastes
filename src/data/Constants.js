454ecb2683b72742fa3b8d0d07220d2e442d7a68
ElixirCalicut <codes.elixir2023@gmail.com>
2026-07-23 21:04:01 +0530

    committed

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -5,7 +5,7 @@
 // export const BASE_URL="http://localhost:8000/client/api/json/v1/1";
 // export const IMAGE_BASEURL="http://localhost:8000";
 export const BASE_URL="https://adblissfultastes.elixirofo.com/client/api/json/v1/1";
-export const IMAGE_BASEURL="https://ad1blissfultastes.elixirofo.com";
+export const IMAGE_BASEURL="https://adblissfultastes.elixirofo.com";
 
 
 
